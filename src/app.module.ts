99c8532e74f49465804fe52d77def4103ba67879
michael19 <rekmixa@gmail.com>
2021-10-09 11:51:15 +0500

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Logger, Module, OnApplicationBootstrap } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+  ],
+})
+export class AppModule implements OnApplicationBootstrap {
+  private logger: Logger = new Logger('AppModule')
+
+  async onApplicationBootstrap(): Promise<void> {}
+}
