12afa2fa8bd2825024937f53ca85f7d5e4e79345
rekmixa <rekmixa@gmail.com>
2022-04-12 18:57:53 +0500

    add logger & upgrade nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Logger, Module, OnApplicationBootstrap } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-  ],
+  imports: [ConfigModule.forRoot()],
 })
 export class AppModule implements OnApplicationBootstrap {
   private logger: Logger = new Logger('AppModule')
 
-  async onApplicationBootstrap(): Promise<void> {}
+  async onApplicationBootstrap(): Promise<void> {
+    this.logger.log('onApplicationBootstrap')
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { Transport } from '@nestjs/microservices'
 import { AppModule } from './app.module'
+import Logger from './services/logger'
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(
     AppModule,
     {
+      logger: new Logger(),
       transport: Transport.TCP,
       options: {
         port: 3000
@@ -13,7 +15,7 @@ async function bootstrap() {
     },
   )
 
-  app.listen(() => console.log('Microservice is listening'))
+  app.listen()
 }
 
 bootstrap()
